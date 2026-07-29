e9f8656d8362eb49ddf4196733f7ab46a5abe8b8
Nanashi <dattran1203nd@proton.me>
2026-07-29 19:38:43 +0700

    Feat: Reimplemented SKN and VTCNN2.

--- src/neuralforge/model/signal-processing/automatic-modulation-classification/VTCNN2.py
+++ src/neuralforge/model/signal-processing/automatic-modulation-classification/VTCNN2.py
@@ -1,26 +1,22 @@
 import torch
 
 class VTCNN2(torch.nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
+    def __init__(self, num_classes: int):
+        super().__init__() # type: ignore
 
         self.layers = torch.nn.ModuleList([
-
-            # First convolutional layer:
             torch.nn.Sequential(
                 torch.nn.Conv2d(in_channels=1, out_channels=256, kernel_size=(1, 3)),
                 torch.nn.ReLU(),
                 torch.nn.Dropout(0.5)
             ),
 
-            # Second convolutional layer:
             torch.nn.Sequential(
                 torch.nn.Conv2d(in_channels=256, out_channels=80, kernel_size=(2, 3)),
                 torch.nn.ReLU(),
                 torch.nn.Dropout(0.5)
             ),
 
-            # Flatten layer:
             torch.nn.Sequential(
                 torch.nn.Flatten(),
                 torch.nn.LazyLinear(out_features=256),
@@ -28,13 +24,12 @@ class VTCNN2(torch.nn.Module):
                 torch.nn.Dropout(0.5)
             ),
 
-            # Output layer:
             torch.nn.Sequential(
                 torch.nn.LazyLinear(out_features=num_classes)
             )
         ])
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         for layer in self.layers:
             x = layer(x)
         return x
--- src/neuralforge/neural-network/convolution/SKConv2d.py
+++ src/neuralforge/neural-network/convolution/SKConv2d.py
@@ -1,24 +1,39 @@
 import torch
 
 class SKConv2d(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, M=2, r=16, L=32, G=1):
-        super().__init__()
-        self.M = M
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        M: int = 2,
+        r: int = 16,
+        L: int = 32,
+        groups: int = 1
+    ):
+
+        super().__init__() # type: ignore
 
         self.convs = torch.nn.ModuleList([
             torch.nn.Sequential(
-                torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, 
-                                padding=1+i, dilation=1+i, bias=False, groups=G),
+                torch.nn.Conv2d(
+                    in_channels = in_channels,
+                    out_channels = out_channels,
+                    kernel_size = 3,
+                    stride = stride,
+                    padding = 1 + i,
+                    dilation = 1 + i,
+                    groups = groups,
+                    bias = False
+                ),
                 torch.nn.BatchNorm2d(out_channels),
                 torch.nn.ReLU(inplace=True)
-            )
-            for i in range(M)
+            ) for i in range(M)
         ])
 
         self.gap = torch.nn.AdaptiveAvgPool2d((1, 1))
 
-        d = max(int(out_channels / r), L)
-
+        d = max(out_channels // r, L)
         self.fc = torch.nn.Sequential(
             torch.nn.Linear(out_channels, d),
             torch.nn.BatchNorm1d(d),
@@ -31,41 +46,115 @@ class SKConv2d(torch.nn.Module):
 
         self.softmax = torch.nn.Softmax(dim=1)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         batch_size = x.size(0)
 
-        feats = [f(x) for f in self.convs]
+        threads = [f(x) for f in self.convs] # List of [B, out_channels, ...shape]
+        feats = torch.stack(threads, dim=1) # [B, M, out_channels, ...shape]
 
-        feats_tensor = torch.stack(feats, dim=1)
+        U = feats.sum(dim=1) # [B, M, out_channels, ...shape] -> [B, out_channels, ...shape]
+        Z = self.fc(self.gap(U).view(batch_size, -1)) # [B, out_channels, ...shape] -> [B, out_channels, 1, 1] -> [B, out_channels] -> [B, d]
 
-        U = feats_tensor.sum(dim=1)
-        Aux = self.gap(U).view(batch_size, -1)
-        Z = self.fc(Aux)
+        weights = torch.stack([fc(Z) for fc in self.fcs], dim=1) # [B, d] -> [B, out_channels] -> [B, M, out_channels]
+        weights = self.softmax(weights).unsqueeze(-1).unsqueeze(-1) # [B, M, out_channels, 1, 1]
 
-        weights = torch.stack([fc(Z) for fc in self.fcs], dim=1)
-        weights = self.softmax(weights)
-
-        weights = weights.unsqueeze(-1).unsqueeze(-1) 
-
-        res = (feats_tensor * weights).sum(dim=1)
+        res = (feats * weights).sum(dim=1) # [B, M, out_channels, ...shape] -> [B, out_channels, ...shape]
 
         return res
 
 class SKUnit(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, M=2, r=16, L=32, G=1):
-        super().__init__()
-        self.conv = SKConv2d(in_channels, out_channels, stride, M, r, L, G)
-        self.bn = torch.nn.BatchNorm2d(out_channels)
-        self.relu = torch.nn.ReLU(inplace=True)
-        
+    is_preact = True
+
+    def __init__(
+        self,
+        in_channels: int,
+        intermediate_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        M: int = 2,
+        r: int = 16,
+        L: int = 32,
+        groups: int = 1,
+        mode: str = "v2"
+    ):
+        super().__init__() # type: ignore
+
+        if mode not in ["v1", "v2"]:
+            raise NotImplementedError(f"Current mode '{mode}' is not available for SKUnit.")
+
         self.shortcut = torch.nn.Sequential()
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = torch.nn.Sequential(
-                torch.nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+            if mode == 'v1':
+                self.shortcut = torch.nn.Sequential(
+                    torch.nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                    torch.nn.BatchNorm2d(out_channels)
+                )
+            else:
+                self.shortcut = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+
+        if mode == 'v1':
+            self.is_preact = False
+
+            self.project = torch.nn.Sequential(
+                torch.nn.Conv2d(in_channels, intermediate_channels, 1, bias=False),
+                torch.nn.BatchNorm2d(intermediate_channels),
+                torch.nn.ReLU(inplace=True)
+            )
+
+            self.transform = torch.nn.Sequential(
+                SKConv2d(
+                    intermediate_channels,
+                    intermediate_channels,
+                    stride = stride,
+                    M = M,
+                    r = r,
+                    L = L,
+                    groups = groups
+                ),
+                torch.nn.BatchNorm2d(intermediate_channels),
+                torch.nn.ReLU(inplace=True)
+            )
+
+            self.lift = torch.nn.Sequential(
+                torch.nn.Conv2d(intermediate_channels, out_channels, 1, bias=False),
                 torch.nn.BatchNorm2d(out_channels)
             )
-    
-    def forward(self, x):
-        res = self.bn(self.conv(x))
-        res += self.shortcut(x)
-        return self.relu(res)
+
+        else:
+            self.project = torch.nn.Sequential(
+                torch.nn.BatchNorm2d(in_channels),
+                torch.nn.ReLU(inplace=True),
+                torch.nn.Conv2d(in_channels, intermediate_channels, 1, bias=False)
+            )
+
+            self.transform = torch.nn.Sequential(
+                torch.nn.BatchNorm2d(intermediate_channels),
+                torch.nn.ReLU(inplace=True),
+                SKConv2d(
+                    intermediate_channels,
+                    intermediate_channels,
+                    stride = stride,
+                    M = M,
+                    r = r,
+                    L = L,
+                    groups = groups
+                )
+            )
+
+            self.lift = torch.nn.Sequential(
+                torch.nn.BatchNorm2d(intermediate_channels),
+                torch.nn.ReLU(inplace=True),
+                torch.nn.Conv2d(intermediate_channels, out_channels, 1, bias=False)
+            )
+
+        self.final_ReLU = torch.nn.ReLU(inplace=True)
+        self.mode = mode
+
+    def forward(self, x: torch.Tensor):
+        residual = self.shortcut(x)
+
+        x = self.project(x)
+        x = self.transform(x)
+        x = self.lift(x)
+
+        return self.final_ReLU(x + residual) if self.mode == 'v1' else x + residual
